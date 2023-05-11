597a4951be81ac3f9ff18fe47064ba403b472762
Marshall <mn.acunab@gmail.com>
2023-05-11 11:03:06 -0300

    add abstract common routes manager

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ const dotenvResult = dotenv.config();
 if(dotenvResult.error) {
     throw dotenvResult.error;
 }
+import DbConfig from './db/DbConfig';
 import express, {Application, Request, Response} from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import passport from 'passport';
+import { CommonRoutesConfig } from './helpers/CommonRoutesConfig';
 
+DbConfig.connect(process.env.DB_CNN as string);
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app: Application = express();
+const routes: Array<CommonRoutesConfig> = [];
 
 app.use(helmet());
 app.use(cors());
