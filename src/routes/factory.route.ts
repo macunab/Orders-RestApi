48ef0428e60b11485a999d15d751b0f56ad2d97b
Marshall <mn.acunab@gmail.com>
2023-05-26 10:01:13 -0300

    Factory crud route

--- src/routes/factory.route.ts
+++ src/routes/factory.route.ts
@@ -1,5 +1,9 @@
-import { Application } from "express";
+import { Application, NextFunction, Request, Response } from "express";
 import { CommonRoutesConfig } from "../helpers/CommonRoutesConfig";
+import passport from "passport";
+import FactoryController from "../controllers/FactoryController";
+import { check } from "express-validator";
+import ValidationFields from "../middlewares/ValidationFields";
 
 
 export class FactoryRoute extends CommonRoutesConfig {
@@ -9,6 +13,41 @@ export class FactoryRoute extends CommonRoutesConfig {
     }
 
     configureRoutes(): Application {
+
+        this.app.route('/factories')
+            .all((req: Request, res: Response, next: NextFunction) => {
+                next();
+            })
+            .get(
+                passport.authenticate( 'jwt', { session: false }),
+                FactoryController.findAllFactories
+            )
+            .post(
+                passport.authenticate('jwt', { session: false }),
+                check('name', 'The name is required').not().isEmpty(),
+                check('contact', 'The contact is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                FactoryController.createFactory
+            );
+
+            this.app.route('/factories/:id')
+                .all((req:Request, res: Response, next: NextFunction) => {
+                    next();
+                })
+                .put(
+                    passport.authenticate('jwt', { session: false }),
+                    check('id', 'The ID param is required').not().isEmpty(),
+                    check('name', 'The name is required').not().isEmpty(),
+                    check('contact', 'The contact is required').not().isEmpty(),
+                    ValidationFields.verifyErrors,
+                    FactoryController.updateFactory
+                )
+                .delete(
+                    passport.authenticate('jwt', { session: false }),
+                    check('id', 'The ID param is required').not().isEmpty(),
+                    ValidationFields.verifyErrors,
+                    FactoryController.deleteFactory
+                );
 
         return this.app;
     }
