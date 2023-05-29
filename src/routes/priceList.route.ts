00f29feb311c5981f199190651b75176b008336f
Marshall <mn.acunab@gmail.com>
2023-05-29 14:22:28 -0300

    priceList and transport routes

--- src/routes/priceList.route.ts
+++ src/routes/priceList.route.ts
@@ -1,5 +1,9 @@
-import { Application } from "express";
+import { Application, NextFunction, Request, Response } from "express";
 import { CommonRoutesConfig } from "../helpers/CommonRoutesConfig";
+import passport from "passport";
+import PriceListController from "../controllers/PriceListController";
+import { check } from "express-validator";
+import ValidationFields from "../middlewares/ValidationFields";
 
 
 export class PriceListRoute extends CommonRoutesConfig {
@@ -9,6 +13,39 @@ export class PriceListRoute extends CommonRoutesConfig {
     }
 
     configureRoutes(): Application {
+
+        this.app.route('/priceLists')
+            .all((req: Request, res: Response, next: NextFunction) => {
+                next();
+            })
+            .get(
+                passport.authenticate('jwt', { session: false }),
+                PriceListController.findAllPriceLists
+            )
+            .post(
+                passport.authenticate('jwt', { session: false }),
+                check('name', 'The name is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                PriceListController.createPriceList
+            );
+
+        this.app.route('/priceLists/:id')
+            .all((req: Request, res: Response, next: NextFunction) => {
+                next();
+            })
+            .put(
+                passport.authenticate('jwt', { session: false }),
+                check('id', 'The param ID is required').not().isEmpty(),
+                check('name', 'The name is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                PriceListController.updatePriceList
+            )
+            .delete(
+                passport.authenticate('jwt', { session: false }),
+                check('id', 'The param ID is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                PriceListController.deletePriceList
+            );
 
         return this.app;
     }
--- src/routes/transport.route.ts
+++ src/routes/transport.route.ts
@@ -1,14 +1,50 @@
-import { Application } from "express";
+import { Application, NextFunction, Request, Response } from "express";
 import { CommonRoutesConfig } from "../helpers/CommonRoutesConfig";
+import passport from "passport";
+import TransportController from "../controllers/TransportController";
+import { check } from "express-validator";
+import ValidationFields from "../middlewares/ValidationFields";
 
 
 export class TransportRoute extends CommonRoutesConfig {
-    
+
     constructor(app: Application) {
         super(app, 'TransportRoutes');
     }
     configureRoutes(): Application {
-        
+
+        this.app.route('/transports')
+            .all((req: Request, res: Response, next: NextFunction) => {
+                next();
+            })
+            .get(
+                passport.authenticate('jwt', { session: false }),
+                TransportController.findAllTransports
+            )
+            .post(
+                passport.authenticate('jwt', { session: false }),
+                check('name', 'The name is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                TransportController.createTransport
+            );
+
+        this.app.route('/transports/:id')
+            .all((req: Request, res: Response, next: NextFunction) => {
+                next();
+            })
+            .put(
+                passport.authenticate('jwt', { session: false }),
+                check('id', 'The param ID is required').not().isEmpty(),
+                check('name', 'The name is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                TransportController.updateTransport
+            )
+            .delete(
+                passport.authenticate('jwt', { session: false }),
+                check('id', 'The param ID is required').not().isEmpty(),
+                ValidationFields.verifyErrors,
+                TransportController.deleteTransport
+            );
         return this.app;
     }
 
